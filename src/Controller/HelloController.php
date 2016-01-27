e70566a53a8e99da4923c571bef902d40c6d133f
Michael Anello <michael@anelloconsulting.com>
2016-01-27 10:30:39 -0500

    Initial commit.

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\hello_world\Controller\HelloController.
+ */
+
+namespace Drupal\hello_world\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Class HelloController.
+ *
+ * @package Drupal\hello_world\Controller
+ */
+class HelloController extends ControllerBase {
+  /**
+   * Hello.
+   *
+   * @return string
+   *   Return Hello string.
+   */
+  public function hello() {
+    return [
+        '#type' => 'markup',
+        '#markup' => $this->t('Implement method: hello')
+    ];
+  }
+
+}
